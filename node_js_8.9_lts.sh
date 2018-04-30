4b07497ee8641ba60f2a2745ec4a4850034af80d
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2018-04-30 19:29:39 +0600

    yarn added

--- node_js_8.9_lts.sh
+++ node_js_8.9_lts.sh
@@ -6,4 +6,8 @@ echo "Setting up Node.js 8.* LTS"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
+echo "Installing Yarn"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install -y yarn
 echo "Thank you"
