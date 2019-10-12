c057609cc48951202b4b298ba1ab78cff8566015
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2019-10-12 15:45:22 +0600

    updated

--- deployadminer.sh
+++ deployadminer.sh
@@ -13,39 +13,10 @@ sudo mv latest-mysql-en.php db.php
 echo -e "Dowloaded and Renamed Successfully\n"
 
 echo "Creating a Configuration File for Adminer in sites-enabled directory"
-sudo cat > /etc/nginx/sites-enabled/db-adminer <<EOF
-
-server {
-  listen 51;
-  listen [::]:51;
- 
-  root /var/www/html/;
-  index db.php
-
-  location / {
-    try_files $uri $uri/ =404;
-  }
-
-  error_page 404 /404.html;
-  error_page 500 502 503 504 /50x.html;
-
-  location = /50x.html {
-    root /usr/share/nginx/html;
-  }
-
-  location ~ \.php$ {
-    fastcgi_pass unix:/run/php/php7.2-fpm.sock;
-    #fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    #include fastcgi_params;
-    #include snippets/fastcgi-php.conf;
-  }
-
-  location ~ /\.ht {
-    deny all;
-  }
-}
-
-EOF
+sudo wget https://raw.githubusercontent.com/masiur/Environment-Configuration/master/src/db-adminer
+sudo ln -s /etc/nginx/sites-available/db-adminer /etc/nginx/sites-enabled/
+sudo systemctl reload nginx
+echo "Configuration File Ready"
 
 sudo nginx -t
 sudo systemctl reload nginx
