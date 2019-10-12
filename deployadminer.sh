01803b64471a0cd3610b3b880837715b317979ca
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2019-10-12 16:03:07 +0600

    bug fixed

--- deployadminer.sh
+++ deployadminer.sh
@@ -8,14 +8,16 @@ sudo apt-get update
 echo -e "System Update Completed Successfully\n"
 
 echo -e "Downloading latest Adminer\n"
-sudo wget https://www.adminer.org/latest-mysql-en.php
-sudo mv latest-mysql-en.php db.php
+cd /var/www/html
+sudo wget -q -O db.php https://www.adminer.org/latest-mysql-en.php
 echo -e "Dowloaded and Renamed Successfully\n"
 
 echo "Creating a Configuration File for Adminer in sites-enabled directory"
+cd /etc/nginx/sites-available
 sudo wget https://raw.githubusercontent.com/masiur/Environment-Configuration/master/src/db-adminer
 sudo ln -s /etc/nginx/sites-available/db-adminer /etc/nginx/sites-enabled/
 sudo systemctl reload nginx
+cd ~
 echo "Configuration File Ready"
 
 sudo nginx -t
