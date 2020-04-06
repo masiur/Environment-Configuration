4948024e149cd9e3e768da16cade9bc609480faa
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2020-04-07 04:24:19 +0600

    updated

--- lemp_deb_18.04.sh
+++ lemp_deb_18.04.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-echo "Welcome LEMP Stack Setup (Ubuntu 18.04) 1.0"
+echo "Welcome automated LEMP Stack Setup (Ubuntu 18.04) 1.2"
 
-echo "Steap:1 [System Update]"
+echo "Step:1 [System Update]"
 echo "Update Starts....."
-sudo apt-get update
+sudo apt update && sudo apt upgrade
 echo -e "System Update Completed Successfully\n"
 
 echo "Step:2 [Install NGINX]"
-sudo apt-get install -y nginx
+sudo apt install -y nginx
 echo -e "NGINX Installation Completed Successfully\n"
 
-echo "Step:3 [Install MySQL]"
+
+echo "Step:3 [Allow SSH in Firewall]"
+sudo ufw allow OpenSSH
+echo -e "Done."
+
+echo "Step:4 [Allow nginx in Firewall]"
+sudo ufw allow in "Nginx Full"
+echo -e "Done."
+
+echo "Copying IP of this server"
+ip=`curl -s https://api.ipify.org`
+
+echo "Step:4 [Allow nginx in Firewall]"
 sudo apt-get install -y mysql-server mysql-client libmysqlclient-dev
 echo -e "MySQL Installation Completed Successfully\n"
 
-echo "Step:4 [Install PHP7.2]"
+echo "Step:5 [Install PHP7.2]"
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php -y 
 sudo apt-get -y update
@@ -36,42 +48,73 @@ sudo systemctl restart nginx
 
 echo "Remove the default symlink in sites-enabled directory"
 sudo rm /etc/nginx/sites-enabled/default
-cat > /etc/nginx/sites-enabled/default <<EOF
 
-server {
-  listen 80;
-  listen [::]:80;
-  server_name localhost;
-  root /usr/share/nginx/html/;
-  index index.php index.html index.htm index.nginx-debian.html;
 
-  location / {
-    try_files $uri $uri/ =404;
-  }
-
-  error_page 404 /404.html;
-  error_page 500 502 503 504 /50x.html;
-
-  location = /50x.html {
-    root /usr/share/nginx/html;
-  }
-
-  location ~ \.php$ {
-    fastcgi_pass unix:/run/php/php7.2-fpm.sock;
-    #fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    #include fastcgi_params;
-    #include snippets/fastcgi-php.conf;
-  }
-
-  location ~ /\.ht {
-    deny all;
-  }
-}
-
-EOF
+sudo echo 'server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www;
+    index index.php index.html index.htm index.nginx-debian.html;
+    server_name '$ip';
+    location / {
+        try_files $uri $uri/ =404;
+    }
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+    }
+    location ~ /\.ht {
+        deny all;
+    }
+}' > /etc/nginx/sites-available/default
 
 sudo nginx -t
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enable/
 sudo systemctl reload nginx
+
+echo -e "Downloading latest Adminer\n"
+cd /var/www/html
+sudo wget -q -O db.php https://www.adminer.org/latest-mysql-en.php
+echo -e "Dowloaded and Renamed Successfully\n"
+
+echo "Creating a Configuration File for Adminer in sites-enabled directory"
+cd /etc/nginx/sites-available
+sudo wget https://raw.githubusercontent.com/masiur/Environment-Configuration/master/src/db-adminer
+sudo ln -s /etc/nginx/sites-available/db-adminer /etc/nginx/sites-enabled/
+sudo systemctl reload nginx
+sudo ufw allow 50
+cd ~
+
+sudo cat > /var/www/index.html <<END
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx! Looks like your Server is Ready</h1>
+<br/>
+<a href="http://$ip/info.php">Php - info</a>.<br/>
+<br/>
+<a href="http://$ip:50">Database</a>.<br/>
+</body>
+</html>
+END
+
+sudo cat > /var/www/info.php <<END
+<?php
+phpinfo();
+?>
+END
+
+echo "Configuration File Ready"
 
 echo "Step:6 [Install curl]"
 sudo apt-get install -y curl
@@ -83,7 +126,7 @@ sudo apt-get update
 sudo apt-get install -y git
 echo -e "Git Installation Completed Successfully\n"
 
-echo "Step:8 [Install mcrypt and enable rewrite]"
+echo "Step:8 [Install php-fpm]"
 sudo service php7.2-fpm restart
 sudo systemctl restart nginx
 echo -e "mcrypt Installation and enable rewrite Completed Successfully\n" 
@@ -93,5 +136,18 @@ curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 echo -e "Composer Installation Completed Successfully\n"
 
+echo "+-------------------------------------------+"
+echo "|    ${b}${A}Finish Auto Install and Setup LEMP${R}     |"
+echo "|                                           |"
+echo "| Web Site: http://$ip/"
+echo "|                                           |"
+echo "| Database Adminer: http://$ip:50"
+echo "| User:${E}root${R} || Pass:${E}$pass${R}"
+echo "|                                           |"
+echo "| Test PHP:http://$ip/info.php"
+echo "|                                           |"
+echo "|        ${E}Warning:Delete info.php${R}            |"
+echo "|                                           |"
+echo "+-------------------------------------------+"
 
 echo "Thanks  !!! ~ Your Lemp config has been successfully done"
