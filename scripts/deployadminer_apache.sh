e0105328d347b845bafee46e3425d1b615532097
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2020-10-28 14:20:15 +0600

    Update deployadminer_apache.sh

--- scripts/deployadminer_apache.sh
+++ scripts/deployadminer_apache.sh
@@ -2,10 +2,6 @@
 
 echo "Welcome to Lightweight Database UI Management (Adminer for PHP) Setup on LEMP Stack Setup (Ubuntu 16.04)"
 
-echo "System Update"
-echo "Update Starts....."
-sudo apt-get update
-echo -e "System Update Completed Successfully\n"
 
 echo -e "Downloading latest Adminer\n"
 cd /var/www/html
@@ -17,6 +13,8 @@ cd /etc/apache2/sites-available
 sudo wget https://raw.githubusercontent.com/masiur/Environment-Configuration/master/confs/adminer_apache.conf
 sudo a2ensite adminer_apache.conf
 cd ~
+
+sudo ufw allow 9999 #Allowing port in URL
 echo "Configuration File Ready"
 
 sudo apachectl configtest
@@ -24,4 +22,5 @@ sudo systemctl reload apache2
 
 YOURIP=`wget -qO- ifconfig.co`
 
-echo "Thanks  !!! ~ Your DB Management istallation has been successfully done. Visit to http://$YOURIP:9999 or YOURDOMAIN:9999"
+echo "Thanks  !!! ~ Your DB Management istallation has been successfully done. Visit to http://$YOURIP:9999 or YOURDOMAIN:9999\n"
+echo "Don't Forget to Allow 9999 on your Virtual Machine Firewall"
