417cb94ce33fd898150c8f8842a4f33f6867b076
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2020-10-28 14:03:25 +0600

    Update deployadminer_apache.sh

--- scripts/deployadminer_apache.sh
+++ scripts/deployadminer_apache.sh
@@ -2,7 +2,7 @@
 
 echo "Welcome to Lightweight Database UI Management (Adminer for PHP) Setup on LEMP Stack Setup (Ubuntu 16.04)"
 
-echo "Steap:1 [System Update]"
+echo "System Update"
 echo "Update Starts....."
 sudo apt-get update
 echo -e "System Update Completed Successfully\n"
@@ -14,7 +14,7 @@ echo -e "Dowloaded and Renamed Successfully\n"
 
 echo "Creating a Configuration File for Adminer in sites-enabled directory"
 cd /etc/apache2/sites-available
-sudo wget https://raw.githubusercontent.com/masiur/Environment-Configuration/master/src/adminer_apache.conf
+sudo wget https://raw.githubusercontent.com/masiur/Environment-Configuration/master/confs/adminer_apache.conf
 sudo a2ensite adminer_apache.conf
 cd ~
 echo "Configuration File Ready"
@@ -22,5 +22,6 @@ echo "Configuration File Ready"
 sudo apachectl configtest
 sudo systemctl reload apache2
 
+YOURIP=`wget -qO- ifconfig.co`
 
-echo "Thanks  !!! ~ Your DB Management istallation has been successfully done. Visit to YOURIP:50 or YOURDOMAIN:50"
+echo "Thanks  !!! ~ Your DB Management istallation has been successfully done. Visit to http://{$YOURIP}:9999 or YOURDOMAIN:9999"
